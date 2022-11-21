e40f2e823271c5def03350088fee8f11782d37d3
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-21 16:04:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <assert.h>
+#include "ColorPair.cpp"
+#include "test.cpp"
 
 namespace TelCoColorCoder
 {
@@ -9,14 +11,16 @@ namespace TelCoColorCoder
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
     };
+    
     int numberOfMajorColors =
         sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    
     const char* MinorColorNames[] = {
         "Blue", "Orange", "Green", "Brown", "Slate"
     };
+    
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
     
 }
 
