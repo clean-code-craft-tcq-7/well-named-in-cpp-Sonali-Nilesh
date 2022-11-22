4c4ac880150b6e93a493d06ca70756870c5c7279
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-22 07:31:27 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <assert.h>
 #include "ColorPair.cpp"
+#include "ColorPair.hpp"
 
 namespace TelCoColorCoder
 {
 
 void testNumberToPair(int pairNumber,
-    ColorPair::MajorColor expectedMajor,
-    ColorPair::MinorColor expectedMinor)
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
 {
-    ColorPair::colorPair =
+    TelCoColorCoder::ColorPair colorPair =
         TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
@@ -17,8 +18,8 @@ void testNumberToPair(int pairNumber,
 }
 
 void testPairToNumber(
-    ColorPair::MajorColor major,
-    ColorPair::MinorColor minor,
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
 {
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
