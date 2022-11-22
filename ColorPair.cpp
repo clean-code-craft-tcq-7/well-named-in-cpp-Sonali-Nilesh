b0c97dc7d99cc119543d14a44496a706d13dd989
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-22 08:09:21 +0100

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -4,8 +4,6 @@
 
 namespace TelCoColorCoder
 {
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     
